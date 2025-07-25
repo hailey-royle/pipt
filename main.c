0960c114f7c18e7b4dcc4d1d895df307987ff6be
hailey <hailey-royle@proton.me>
2025-07-25 10:49:58 -0400

    rawFileData is now prosessed into pipItems

--- main.c
+++ main.c
@@ -12,35 +12,68 @@
 
 #define MAX_ITEM_DATA_LENGTH 64
 #define MAX_CONNECTIONS 4
-#define MAX_ITEM_LIST 16
+#define MAX_ITEMS 16
 
 struct PiptItem {
     char connections[MAX_CONNECTIONS][MAX_ITEM_DATA_LENGTH];
     char title[MAX_ITEM_DATA_LENGTH];
     char body[MAX_ITEM_DATA_LENGTH];
 };
-struct PiptItem piptItems[MAX_ITEM_LIST];
+struct PiptItem piptItems[MAX_ITEMS];
 
 int main(int argc, char* argv[]) {
 
     if (argc == 2) {
 
         FILE *filePointer;
-        char rawFileData[MAX_ITEM_DATA_LENGTH];
         filePointer = fopen(argv[1], "r");
+        char rawFileData[MAX_ITEM_DATA_LENGTH];
 
         if(filePointer != NULL) {
+
+            int currentItem = -1;
+            int currentItemConnection = 0;
+            int currentItemBodyPosition = 0;
 
             while (fgets(rawFileData, MAX_ITEM_DATA_LENGTH, filePointer)) {
 
                 if (rawFileData[0] == '#') {
-                    //title
+
+                    currentItem++;
+
+                    for (int i = 0; i < MAX_ITEM_DATA_LENGTH; i++) {
+                        if (rawFileData[i] == '\n') {
+                            break;
+                        }
+                        piptItems[currentItem].title[i] = rawFileData[i];
+                    }
+
+                    currentItemConnection = 0;
+                    currentItemBodyPosition = 0;
+
                 } else if (rawFileData[0] == '-') {
-                    //connection
+
+                    for (int i = 0; i < MAX_ITEM_DATA_LENGTH; i++) {
+                        if (rawFileData[i] == '\n') {
+                            break;
+                        }
+                        piptItems[currentItem].connections[currentItemConnection][i] = rawFileData[i];
+                    }
+
+                    currentItemConnection++;
+
                 } else if (rawFileData[0] == '\n') {
-                    //nothing
+
                 } else {
-                    //body
+
+                    for (int i = 0; i < MAX_ITEM_DATA_LENGTH; i++) {
+                        if (rawFileData[i] == '\n') {
+                            break;
+                        }
+                        piptItems[currentItem].body[currentItemBodyPosition] = rawFileData[i];
+                        currentItemBodyPosition++;
+                    }
+
                 }
 
                 printf("%s", rawFileData);
