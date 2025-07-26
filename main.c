b55cfe7285cb56e4ac360228face03091329ca10
hailey <hailey-royle@proton.me>
2025-07-26 13:26:23 -0400

    formatting

--- main.c
+++ main.c
@@ -14,7 +14,6 @@
 #define MAX_ITEM_DATA_LENGTH 255
 #define MAX_CONNECTIONS 4
 #define MAX_ITEMS 16
-
 #define TITLE_MARKER '#'
 #define BODY_MARKER '-'
 #define CONNECTION_MARKER '>'
@@ -31,46 +30,44 @@ int LoadFile(char** path) {
     FILE *filePointer;
     filePointer = fopen(*path, "r");
     char rawFileData[MAX_ITEM_DATA_LENGTH];
+    int currentItem = -1;
+    int currentItemConnection = 0;
+    int currentItemBodyPosition = 0;
+    int currentLineNumber = 0;
 
     if (filePointer != NULL) {
 
-        int currentItem = -1;
-        int currentItemConnection = 0;
-        int currentItemBodyPosition = 0;
-        int currentLineNumber = 0;
-
         while (fgets(rawFileData, MAX_ITEM_DATA_LENGTH, filePointer)) {
-
             currentLineNumber++;
 
             if (rawFileData[0] == TITLE_MARKER) {
-
                 currentItem++;
 
                 for (int i = 0; i < MAX_ITEM_DATA_LENGTH; i++) {
+
                     if (rawFileData[i+2] == '\n') {
                         break;
                     }
                     piptItems[currentItem].title[i] = rawFileData[i+2];
                 }
-
                 currentItemConnection = 0;
                 currentItemBodyPosition = 0;
 
             } else if (rawFileData[0] == CONNECTION_MARKER) {
 
                 for (int i = 0; i < MAX_ITEM_DATA_LENGTH; i++) {
+
                     if (rawFileData[i+2] == '\n') {
                         break;
                     }
                     piptItems[currentItem].connections[currentItemConnection][i] = rawFileData[i+2];
                 }
-
                 currentItemConnection++;
 
             } else if (rawFileData[0] == BODY_MARKER) {
 
                 for (int i = 0; i < MAX_ITEM_DATA_LENGTH; i++) {
+
                     if (rawFileData[i+2] == '\n') {
                         break;
                     }
@@ -81,21 +78,15 @@ int LoadFile(char** path) {
             } else if (rawFileData[0] == '\n') {
 
             } else {
-
                 printf("data on line %d could not be prosessed, starts with %c\n", currentLineNumber, rawFileData[0]);
-
             }
-
         }
-
     fclose(filePointer);
     return currentItem + 1;
 
     } else {
-
         printf("file %s not found\n.", *path);
         return 0;
-
     }
 }
 
@@ -108,13 +99,12 @@ bool streq(char str1[], char str2[]) {
         if (str1[i] == '\0' || str2[i] == '\0') {
             break;
         }
-
         i++;
-
     }
 
     if ( str1[i] == '\0' && str2[i] == '\0') {
         return 1;
+
     } else {
         return 0;
     }
@@ -125,17 +115,13 @@ int main(int argc, char* argv[]) {
     int piptItemCount;
 
     if (argc == 2) {
-
         piptItemCount = LoadFile(&argv[1]);
 
     } else if (argc > 2) {
-
         puts("too many arguments");
 
     } else if (argc < 2) {
-
         puts("no file given");
-
     }
 
     bool piptItemMatrix[piptItemCount][piptItemCount];
@@ -148,6 +134,7 @@ int main(int argc, char* argv[]) {
 
                 if (streq(piptItems[i].connections[j], piptItems[h].title)) {
                     piptItemMatrix[i][h] = true;
+
                 } else {
                     piptItemMatrix[i][h] = false;
                 }
@@ -160,7 +147,6 @@ int main(int argc, char* argv[]) {
         if (piptItems[i].title[0] == '\0') {
             break;
         }
-
         printf("%s\n", piptItems[i].title);
         printf("%s\n", piptItems[i].body);
 
@@ -169,11 +155,8 @@ int main(int argc, char* argv[]) {
             if (piptItems[i].connections[j][0] == '\0') {
                 break;
             }
-
             printf("%s\n", piptItems[i].connections[j]);
         }
     }
-
     return 0;
-
 }
