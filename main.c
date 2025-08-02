46419a6d1aa1c3435a06ec9d7736c42adb7bcd5b
hailey <hailey-royle@proton.me>
2025-08-02 18:51:51 -0400

    added file reading

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
-int MakeGraph(int* count, char* args) {
-    printf("%d\n", *count);
-    return *count;
+#define MAX_ITEM_DATA 255
+
+int MakeGraph(const char* arg) {
+    FILE *file;
+    file = fopen(arg, "r");
+
+    if (file == NULL) {
+        printf("file %s not found\n", arg);
+        return -1;
+    }
+
+    char fileData[MAX_ITEM_DATA];
+
+    while(fgets(fileData, MAX_ITEM_DATA, file)) {
+        printf("%s", fileData);
+    }
+
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
-    int nodeCount = MakeGraph(&argc, *argv);
+    int nodeCount = MakeGraph(argv[1]);
 
     printf("%d\n", nodeCount);
     return 0;
