ff1016dde4ecb721c41c4f553e7a2ccb2cf549ac
hailey-royle <hailey.royle@proton.me>
2025-09-02 12:01:26 -0600

    pipt. added, moved all variabled in program over to it

--- main.c
+++ main.c
@@ -13,20 +13,25 @@
 #define SPACE_CHAR ' '
 #define BACKGROUND_CHAR '.'
 
-struct piptItem {
+struct item {
     char connection[MAX_ITEM_COUNT][MAX_ITEM_DATA_LENGTH];
     char body[MAX_ITEM_BODY_LINES][MAX_ITEM_DATA_LENGTH];
     char title[MAX_ITEM_DATA_LENGTH];
     char top[MAX_ITEM_DATA_LENGTH];
     char bottom[MAX_ITEM_DATA_LENGTH];
     int connected[MAX_ITEM_COUNT];
-    int connectionCount;
     int width;
     int height;
     int x;
     int y;
+    int connectionCount;
 };
-struct piptItem piptItem[MAX_ITEM_COUNT];
+
+struct pipt {
+    struct item item[MAX_ITEM_COUNT];
+    int itemCount;
+};
+struct pipt pipt;
 
 struct stack {
     int top;
@@ -76,18 +81,18 @@ int VerifyArgs(const int argc) {
     return 1;
 }
 
-int LoadFileData(const char* arg) {
+void LoadFileData(const char* arg) {
     FILE *file;
     file = fopen(arg, "r");
     char rawData[MAX_ITEM_DATA_LENGTH];
 
     if (file == NULL) {
         printf("file %s not found\n", arg);
-        return -1;
+        return;
     }
 
-    int itemCount = -1;
-
+    pipt.itemCount = -1;
+    
     while(fgets(rawData, MAX_ITEM_DATA_LENGTH, file)) {
         static int lineNumber = 0;
         static int bodyNumber = 0;
@@ -95,66 +100,64 @@ int LoadFileData(const char* arg) {
         lineNumber++;
         if (strlen(rawData) >= MAX_ITEM_DATA_LENGTH - 1) {
             printf("data overflow on line %d\n", lineNumber);
-            return -1;
+            return;
         }
         if (bodyNumber >= MAX_ITEM_BODY_LINES) {
-            printf("item %d has to many lines in its body, max is %d", itemCount, MAX_ITEM_BODY_LINES);
+            printf("item %d has to many lines in its body, max is %d", pipt.itemCount, MAX_ITEM_BODY_LINES);
         }
         if (rawData[0] == '\n') {
             continue;
         }
         if (rawData[0] == TITLE_MARK) {
-            itemCount++;
+            pipt.itemCount++;
             bodyNumber = 0;
             connectionNumber = 0;
-            strcpy(piptItem[itemCount].title, rawData);
+            strcpy(pipt.item[pipt.itemCount].title, rawData);
         }
         else if (rawData[0] == BODY_MARK) {
-            strcpy(piptItem[itemCount].body[bodyNumber], rawData);
+            strcpy(pipt.item[pipt.itemCount].body[bodyNumber], rawData);
             bodyNumber++;
         }
         else if (rawData[0] == CONNECTION_MARK) {
-            strcpy(piptItem[itemCount].connection[connectionNumber], rawData);
-            piptItem[itemCount].connectionCount++;
+            strcpy(pipt.item[pipt.itemCount].connection[connectionNumber], rawData);
+            pipt.item[pipt.itemCount].connectionCount++;
             connectionNumber++;
         }
         else {
             printf("data on line %d could not be prosessed\n", lineNumber);
             printf("line must start line with %c : title, %c : body, or %c : connection\n", TITLE_MARK, BODY_MARK, CONNECTION_MARK);
             printf("line data : %s\n", rawData);
-            return -1;
+            return;
         }
     }
-
     fclose(file);
-    return itemCount + 1;
 }
 
 //==============================================================
 //  ConnectItems
 //==============================================================
 
-void FindConnection(const int itemCount, const int itemNumber, const int connectionNumber) {
-    piptItem[itemNumber].connection[connectionNumber][0] = TITLE_MARK;
-    for (int i = 0; i < itemCount; i++) {
-        if (piptItem[i].title[0] == '\0') {
+void FindConnection(const int itemNumber, const int connectionNumber) {
+    pipt.item[itemNumber].connection[connectionNumber][0] = TITLE_MARK;
+    for (int i = 0; i <= pipt.itemCount; i++) {
+        if (pipt.item[i].title[0] == '\0') {
             return;
         }
-        if (strcmp(piptItem[itemNumber].connection[connectionNumber], piptItem[i].title) == 0) {
-            piptItem[itemNumber].connected[connectionNumber] = i;
+        if (strcmp(pipt.item[itemNumber].connection[connectionNumber], pipt.item[i].title) == 0) {
+            pipt.item[itemNumber].connected[connectionNumber] = i;
         }
     }
 }
 
-void ConnectConnections(const int itemCount, const int itemNumber) {
-    for (int i = 0; i < itemCount; i++) {
-        FindConnection(itemCount, itemNumber, i);
+void ConnectConnections(const int itemNumber) {
+    for (int i = 0; i <= pipt.itemCount; i++) {
+        FindConnection(itemNumber, i);
     }
 }
 
-void ConnectItems(const int itemCount) {
-    for (int i = 0; i < itemCount; i++) {
-        ConnectConnections(itemCount, i);
+void ConnectItems() {
+    for (int i = 0; i <= pipt.itemCount; i++) {
+        ConnectConnections(i);
     }
 }
 
@@ -165,7 +168,7 @@ void ConnectItems(const int itemCount) {
 int FindItemHeight(const int itemNumber) {
     int height = 0;
     for (int i = 0; i < MAX_ITEM_BODY_LINES; i++) {
-        if (piptItem[itemNumber].body[i][0] == '\0') {
+        if (pipt.item[itemNumber].body[i][0] == '\0') {
             break;
         }
         height++;
@@ -174,51 +177,51 @@ int FindItemHeight(const int itemNumber) {
 }
 
 int FindItemWidth(const int itemNumber) {
-    long unsigned int width = strlen(piptItem[itemNumber].title);
+    long unsigned int width = strlen(pipt.item[itemNumber].title);
     for (int i = 0; i < MAX_ITEM_BODY_LINES; i++) {
-        if (piptItem[itemNumber].body[i][0] == '\0') {
+        if (pipt.item[itemNumber].body[i][0] == '\0') {
             break;
         }
-        if (width < strlen(piptItem[itemNumber].body[i])) {
-            width = strlen(piptItem[itemNumber].body[i]);
+        if (width < strlen(pipt.item[itemNumber].body[i])) {
+            width = strlen(pipt.item[itemNumber].body[i]);
         }
     }
     return width + 1;
 }
 
 void FormatTop(const int itemNumber) {
-    strcpy(piptItem[itemNumber].top, piptItem[itemNumber].title);
-    piptItem[itemNumber].top[0] = CORRNER_CHAR;
-    piptItem[itemNumber].top[1] = HORIZONTAL_CHAR;
-    for (int i = strlen(piptItem[itemNumber].top) - 1; i <= piptItem[itemNumber].width - 2; i++) {
-        piptItem[itemNumber].top[i] = HORIZONTAL_CHAR;
+    strcpy(pipt.item[itemNumber].top, pipt.item[itemNumber].title);
+    pipt.item[itemNumber].top[0] = CORRNER_CHAR;
+    pipt.item[itemNumber].top[1] = HORIZONTAL_CHAR;
+    for (int i = strlen(pipt.item[itemNumber].top) - 1; i <= pipt.item[itemNumber].width - 2; i++) {
+        pipt.item[itemNumber].top[i] = HORIZONTAL_CHAR;
     }
-    piptItem[itemNumber].top[piptItem[itemNumber].width - 1] = CORRNER_CHAR;
+    pipt.item[itemNumber].top[pipt.item[itemNumber].width - 1] = CORRNER_CHAR;
 }
 
 void FormatBody(const int itemNumber) {
-    for (int i = 0; i <= piptItem[itemNumber].height - 2; i++) {
-        piptItem[itemNumber].body[i][0] = VERTICAL_CHAR;
-        piptItem[itemNumber].body[i][1] = SPACE_CHAR;
-        for (int j = strlen(piptItem[itemNumber].body[i]) - 1; j <= piptItem[itemNumber].width - 2; j++) {
-            piptItem[itemNumber].body[i][j] = SPACE_CHAR;
+    for (int i = 0; i <= pipt.item[itemNumber].height - 2; i++) {
+        pipt.item[itemNumber].body[i][0] = VERTICAL_CHAR;
+        pipt.item[itemNumber].body[i][1] = SPACE_CHAR;
+        for (int j = strlen(pipt.item[itemNumber].body[i]) - 1; j <= pipt.item[itemNumber].width - 2; j++) {
+            pipt.item[itemNumber].body[i][j] = SPACE_CHAR;
         }
-        piptItem[itemNumber].body[i][piptItem[itemNumber].width - 1] = VERTICAL_CHAR;
+        pipt.item[itemNumber].body[i][pipt.item[itemNumber].width - 1] = VERTICAL_CHAR;
     }
 }
 
 void FormatBottom(const int itemNumber) {
-    piptItem[itemNumber].bottom[0] = CORRNER_CHAR;
-    for (int i = 1; i <= piptItem[itemNumber].width - 2; i++) {
-        piptItem[itemNumber].bottom[i] = HORIZONTAL_CHAR;
+    pipt.item[itemNumber].bottom[0] = CORRNER_CHAR;
+    for (int i = 1; i <= pipt.item[itemNumber].width - 2; i++) {
+        pipt.item[itemNumber].bottom[i] = HORIZONTAL_CHAR;
     }
-    piptItem[itemNumber].bottom[piptItem[itemNumber].width - 1] = CORRNER_CHAR;
+    pipt.item[itemNumber].bottom[pipt.item[itemNumber].width - 1] = CORRNER_CHAR;
 }
 
-void FormatItems(const int itemCount) {
-    for (int i = 0; i < itemCount; i++) {
-        piptItem[i].height = FindItemHeight(i);
-        piptItem[i].width = FindItemWidth(i);
+void FormatItems() {
+    for (int i = 0; i <= pipt.itemCount; i++) {
+        pipt.item[i].height = FindItemHeight(i);
+        pipt.item[i].width = FindItemWidth(i);
         FormatTop(i);
         FormatBody(i);
         FormatBottom(i);
@@ -239,13 +242,13 @@ void PossitionItemsY() {
     while (stackY.top >= 0) {
         int connection = -1;
         if (connection >= 0) {
-            currentY += piptItem[stackY.top].height + 1;
+            currentY += pipt.item[stackY.top].height + 1;
             StackPush(&stackY, connection);
         }
         if (connection == -1) {
-            piptItem[stackY.top].y = currentY;
-            printf("item: %s, y %d\n", piptItem[stackY.top].top, piptItem[stackY.top].y);
-            currentY -= piptItem[stackY.top].height;
+            pipt.item[stackY.top].y = currentY;
+            printf("item: %s, y %d\n", pipt.item[stackY.top].top, pipt.item[stackY.top].y);
+            currentY -= pipt.item[stackY.top].height;
             StackPop(&stackY);
         }
     }
@@ -256,20 +259,19 @@ void PossitionItemsY() {
 //  PrintItems
 //==============================================================
 
-void PrintItems(const int itemCount) {
-    for (int i = 0; i < itemCount; i++) {
-        printf("\nx:%d y:%d\n", piptItem[i].x, piptItem[i].y);
-        for (int j = 0; j < piptItem[i].connectionCount; j++) {
-            printf("connection:%d\n", piptItem[i].connected[j]);
+void PrintItems() {
+    for (int i = 0; i <= pipt.itemCount; i++) {
+        printf("\nx:%d y:%d\n", pipt.item[i].x, pipt.item[i].y);
+        for (int j = 0; j < pipt.item[i].connectionCount; j++) {
+            printf("connection:%d\n", pipt.item[i].connected[j]);
         }
-        printf("%s\n", piptItem[i].top);
-        for (int j = 0; j < piptItem[i].height - 2; j++) {
-            printf("%s\n", piptItem[i].body[j]);
+        printf("%s\n", pipt.item[i].top);
+        for (int j = 0; j < pipt.item[i].height - 2; j++) {
+            printf("%s\n", pipt.item[i].body[j]);
         }
-        printf("%s\n", piptItem[i].bottom);
+        printf("%s\n", pipt.item[i].bottom);
     }
 }
-
 
 //==============================================================
 //  main
@@ -279,16 +281,19 @@ int main(int argc, char* argv[]) {
     const int argvPath = VerifyArgs(argc);
     if (argvPath != 1) return -1;
 
-    const int itemCount = LoadFileData(argv[argvPath]);
-    if (itemCount <= 0) return -1;
+    LoadFileData(argv[argvPath]);
+    if (pipt.itemCount < 0) {
+        printf("error loading file data, item count:%d\n", pipt.itemCount);
+        return -1;
+    }
 
-    ConnectItems(itemCount);
+    ConnectItems(pipt.itemCount);
 
-    FormatItems(itemCount);
+    FormatItems(pipt.itemCount);
 
     //PossitionItemsY();
 
-    PrintItems(itemCount);
+    PrintItems(pipt.itemCount);
 
     return 0;
 }
